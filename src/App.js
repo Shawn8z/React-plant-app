cfad6278a56873f190f899188afcb68a0f61582e
Shawn <lcorel86@gmail.com>
2022-08-09 02:16:34 -0500

    adding bootstrap for main page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-
 import './App.css';
+import Button from "react-bootstrap/Button";
+import Card from 'react-bootstrap/Card';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
 
-      <div class="navbar">
-        <h1>nav bar component</h1>
+      <div class="navbar d-flex flex-column flex-md-row align-items-center p-3 mb-4 px-md-4 bg-white border-bottom shadow-sm">
+        <h3 class="my-0 mr-md-auto font-weight-normal">Grow App</h3>
+        <nav class="my-2 my-sm-0 mr-md-3">
+          <a class="p-2 " href="#">Register</a>
+          <span>  |  </span>
+          <a class="p-2 " href="#">login</a>
+        </nav>
+        
       </div>
 
-      <div class="info-block">
+      <div class="info-block px-3 py-3 pt-md-5 pd-md-4 mx-auto text-center">
         <h1>Info block component</h1>
       </div>
 
