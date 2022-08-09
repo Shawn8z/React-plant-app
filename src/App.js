c1757169bef026feab15ad16498869ba147e4cc3
Shawn <lcorel86@gmail.com>
2022-08-09 05:01:00 -0500

    card layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import Button from "react-bootstrap/Button";
-import Card from 'react-bootstrap/Card';
+
 import "bootstrap/dist/css/bootstrap.min.css"
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function App() {
   return (
@@ -14,20 +16,51 @@ function App() {
           <span>  |  </span>
           <a class="p-2 " href="#">login</a>
         </nav>
-        
       </div>
 
       <div class="info-block px-3 py-3 pt-md-5 pd-md-4 mx-auto text-center">
-        <h1>Info block component</h1>
+        <h1 class="display-4">About Us</h1>
+        <p class="lead">
+          "Here at Grow, we inform you with all the necessary knowledge you need
+          to take greate care of the plants around you. Find the plant you are after, 
+          and get all the key knowledge that will help you to take care of your green friend.
+          Also feel free to check out what others are growing in their garden."
+        </p>
       </div>
+      
+      <div class="container">
 
-      <div class="garden">
-        <div>
-          <h1>List of users garden component</h1>
+
+        <div class="card-deck text-center">
+
+            <div class="card m">
+              <p>Card 1</p>
+            </div>
+
+
+            <div class="card">
+              <p>Card 2</p>
+            </div>
+
+
+            <div class="card">
+              <p>Card 3</p>
+            </div>
+            
         </div>
-      </div>
+        
+        
 
-    </div>
+      </div>
+        
+
+
+
+
+
+
+
+  </div>
   );
 }
 
