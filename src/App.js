e72e89c82ce01d67b54cb48d5c331d0a966e6bf6
Shawn <lcorel86@gmail.com>
2022-08-10 02:13:06 -0500

    adding bootstrap modal checkpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import './App.css';
-
+import React, {useState} from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Modal from "./Components/modal";
+
+
+
 
 function App() {
+
+  const [openModal, setOpenModal] = useState(false);
+  
   return (
     <div className="App">
 
-      <div class="navbar d-flex flex-column flex-md-row align-items-center p-3 mb-4 px-md-4 bg-white border-bottom shadow-sm">
-        <h3 class="my-0 mr-md-auto font-weight-normal">Grow App</h3>
-        <nav class="my-2 my-sm-0 mr-md-3">
-          <a class="p-2 " href="#">Register</a>
+      
+
+      <div className="navbar d-flex flex-column flex-md-row align-items-center p-3 mb-4 px-md-4 bg-white border-bottom shadow-sm">
+        <h3 className="my-0 mr-md-auto font-weight-normal">Grow App</h3>
+        <nav className="my-2 my-sm-0 mr-md-3">
+          <a className="p-2 " href="http://localhost:3000/">Register</a>
           <span>  |  </span>
-          <a class="p-2 " href="#">login</a>
+          <a className="p-2 " href="http://localhost:3000/">login</a>
         </nav>
       </div>
 
-      <div class="info-block px-3 py-3 pt-md-5 pd-md-4 mx-auto text-center">
-        <h1 class="display-4">About Us</h1>
-        <p class="lead">
+      {/* <button 
+        className="openModalBtn" 
+          onClick={() => {setOpenModal(!openModal);}
+        }
+      >Open</button> */}
+
+      {/* {openModal && <Modal closeModal={setOpenModal}/>} */}
+
+      <div className="info-block px-3 py-3 pt-md-5 pd-md-4 mx-auto text-center">
+        <h1 className="display-4">About Us</h1>
+        <p className="lead">
           "Here at Grow, we inform you with all the necessary knowledge you need
           to take greate care of the plants around you. Find the plant you are after, 
           and get all the key knowledge that will help you to take care of your green friend.
@@ -28,39 +42,45 @@ function App() {
         </p>
       </div>
       
-      <div class="container">
+      <div className="container col-md-8 mt-5 text-center">
+        <h2 className="display-4">Gardens</h2>
 
+        <div className="row mt-4 mx-auto">
 
-        <div class="card-deck text-center">
-
-            <div class="card m">
-              <p>Card 1</p>
+          <div className="col-sm-4">
+            <div className="card">
+              <img src={require("./img/placeholder-image.jpg")} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Garden Name1</h5>
+                <p className="card-text">Garden Detail</p>
+              </div>
             </div>
+          </div>
 
-
-            <div class="card">
-              <p>Card 2</p>
+          <div className="col-sm-4">
+            <div className="card">
+              <img src={require("./img/placeholder-image.jpg")} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Garden Name2</h5>
+                <p className="card-text">Garden Detail</p>
+              </div>
             </div>
+          </div>
 
-
-            <div class="card">
-              <p>Card 3</p>
+          <div className="col-sm-4">
+            <div className="card">
+              <img src={require("./img/placeholder-image.jpg")} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Garden Name2</h5>
+                <p className="card-text">Garden Detail</p>
+              </div>
             </div>
-            
+          </div>
         </div>
         
-        
-
       </div>
-        
 
-
-
-
-
-
-
-  </div>
+    </div>
   );
 }
 
