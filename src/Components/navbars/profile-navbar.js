35a4415ec1082b92ec9d0b0893e68bc3b0f6dd80
Shawn <lcorel86@gmail.com>
2022-08-12 00:55:21 -0500

    search and filter styling finished

--- src/Components/navbars/profile-navbar.js
+++ src/Components/navbars/profile-navbar.js
@@ -1,81 +1,127 @@
-import React, { useState, useRef } from 'react';
+// import React, { useState, useRef } from 'react';
 
 import Form from "react-bootstrap/Form";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import Overlay from 'react-bootstrap/Overlay';
-import Tooltip from 'react-bootstrap/Tooltip';
 
-import TooltipContent from '../tooltipContent';
-import Popover from 'react-bootstrap/Popover';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Accordion from 'react-bootstrap/Accordion';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
 
-const popover = (
-    <Popover id = "popover-basic">
-        <Popover.Header as="h3">Popover bottom</Popover.Header>
-        <Popover.Body>
-        
-           <div className="d-flex ">
+function CustomToggle({ children, eventKey }) {
+    const decoratedOnClick = useAccordionButton(eventKey, () => 
+        console.log("totally custom!"),
+    );
 
-                {["Name", "Family", "Water", "MatureSize"].map((item) => (
-                <div key={`inline-${item}`} >
-                    <Form.Check inline type="checkbox" label={item} id={`inline-${item}`} />
-                </div>
-            ))}
-           </div>
+    return (
+        <Button
+            variant="light"
+            onClick={decoratedOnClick}
+        >
+            {children}
+        </Button>
+    )
+}
 
-        </Popover.Body>
-    </Popover>
-);
 
 function ProfileNavbar() {
-    const [showOption, setShowOption] = useState(false);
-    const target = useRef(null);
+
+
+    
 
     return (
         <div>
-            <div className="navbar d-flex flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom">
+            <div className="navbar d-flex flex-md-row p-3 px-md-4 bg-white border-bottom">
 
                 <h3 className="my-0 mr-md-auto font-weight-normal">Grow App</h3>
-                <nav className="my-2 my-sm-0 mr-md-3">
-                    <Button variant="light" >
-                        XXX
+                <nav className="my-2 my-sm-0 mr-md-3 ">
+                    <Button variant="light" ClassName="col-md-6" >
+                        Add Plant
                     </Button>
                     <span>  |  </span>
                     <Button variant="light" >
-                        XXX
+                        LogOut
                     </Button>
                 </nav>
                 
                 
             </div>
-            <div className="search-bar d-flex justify-content-center p-3 mb-4 px-md-4 bg-white border-bottom shadow-sm">
-                
-                    <Form className="d-flex">
-                        <Form.Control 
-                            type="search" 
-                            placeholder="Search..."
-                            className="me-2" 
-                        />
-                        <ButtonGroup>
-                            <Button variant="light" type="submit">Search</Button>
-                            <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
-                                <Button variant="light" 
-                                        type="button" 
-                                        ref={target} 
-                                        onClick={()=>{setShowOption(!showOption)}}
-                                >
-                                        Filter
-                                </Button>
-                            </OverlayTrigger>
-                        </ButtonGroup>
 
-                    </Form>
-                </div>
+            <div className="search-bar d-flex flex-row p-3 mb-4 px-md-4 bg-white border-bottom shadow-sm">
+
+                    <Accordion defaultActiveKey="0" className="col-md-6 mx-auto">
+                        <Form className="align-items-center">
+                            <div className="search-bar d-flex flex-row mx-auto">
+                                <Form.Control 
+                                    type="search" 
+                                    placeholder="Search..."
+                                    className="me-2" 
+                                />
+                                
+                                <ButtonGroup className="col-4">
+                                    <Button variant="light" type="submit">Search</Button>
+                                    <CustomToggle eventKey="1">Filter</CustomToggle>
+                                </ButtonGroup>
+                            </div>
+
+                            <Accordion.Collapse eventKey="1">
+                                <div className="d-flex flex-row justify-content-center mx-auto mt-3">
+                                    {[  
+                                        "Name", 
+                                        "Family", 
+                                        "Hardiness",
+                                        "Water"
+                                    ].map( (item) => (
+                                        item === "Name" ? 
+                                        <div key={`${item} checkbox`}>
+                                            <Form.Check
+                                                inline
+                                                label={item}
+                                                type="checkbox"
+                                                name={item}
+                                                checked
+                                            />
+                                        </div> 
+                                        :
+                                        <div key={`${item} checkbox`}>
+                                            <Form.Check
+                                                inline
+                                                label={item}
+                                                type="checkbox"
+                                                name={item}
+                                            />
+                                        </div>
+                                        ))}
+                                </div>
+                            </Accordion.Collapse>
+                            <Accordion.Collapse eventKey="1">
+                                <div className="d-flex flex-row justify-content-center mx-auto mt-3">
+                                    {[
+                                        "Mature Size", 
+                                        "Soil Type", 
+                                        "Sun Exposure",
+                                    ].map( (item) => (
+                                        <div key={`${item} checkbox`}>
+                                            <Form.Check
+                                                inline
+                                                label={item}
+                                                type="checkbox"
+                                                name={item}
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
+                            </Accordion.Collapse>
+                            
+                        </Form>
+                    </Accordion>
+
             </div>
+
+        </div>
+
+
 
     )
 }
