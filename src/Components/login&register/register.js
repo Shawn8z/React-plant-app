20f2145ae5399f83549955ebff6301370517dc43
Shawn <lcorel86@gmail.com>
2022-08-27 17:38:05 -0500

    adding registration

--- src/Components/login&register/register.js
+++ src/Components/login&register/register.js
@@ -1,13 +1,44 @@
+import { useState } from 'react';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase-config';
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function Register() {
 
+    const [email, setEmail] = useState("");
+    const [passWord, setPassWord] = useState("");
+    const [passWordCheck, setPassWordCheck] = useState("");
+
+    const handleEmail = (event) => {
+        setEmail(event.target.value);
+    }
+    const handlePassWordChange = (event) => {
+        setPassWord(event.target.value);
+    }
+    const handlePassWordCheckChange = (event) => {
+        setPassWordCheck(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        createUserWithEmailAndPassword(auth, email, passWord)
+            .then((userObj) => {
+                const user = userObj;
+                console.log(user);
+                console.log(user.uid)
+            })
+    }
+
     return(
-        <Form>
+        <Form onSubmit={handleSubmit}>
+
             <Form.Group className="mb-3" controlId="formEmail">
-                <Form.Label>Register</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Label>Email</Form.Label>
+
+                <Form.Control type="email" placeholder="Enter email" onChange={handleEmail} required />
+
+
                 <Form.Text className="text-muted">
                     Create your new account
                 </Form.Text>
@@ -15,12 +46,17 @@ function Register() {
             
             <Form.Group className="mb-3" controlId="formPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+
+                <Form.Control type="password" placeholder="Password" onChange={handlePassWordChange} required />
+
+
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formRepeatPassword">
                 <Form.Label>Repeat Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+
+                <Form.Control type="password" placeholder="Password" onChange={handlePassWordCheckChange} required />
+
             </Form.Group>
             <Button variant="primary" type="submit">
                 Submit
--- src/Components/modals/register-modal.js
+++ src/Components/modals/register-modal.js
@@ -2,33 +2,28 @@ import { logDOM } from '@testing-library/react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Register from '../login&register/register';
-import Login from '../login&register/login';
 
 function RegisterModal(props) {
     
     return (
         <Modal
             {...props}
-            size="lg"
+            size="md"
             centered
         >
 
             <Modal.Header closeButton>
                 <Modal.Title id="register-modal">
-                    Register
+                    Register Here
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h4>Register Modal</h4>
-                <p>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                consectetur ac, vestibulum at eros.
-                </p>
+
                 <Register />
+
             </Modal.Body>
             <Modal.Footer>
-                <button onClick={props.onHide}>Close</button>
+                <Button onClick={props.onHide}>Close</Button>
             </Modal.Footer>
         </Modal>
 
